a9f2f9d3e22f00434dadefe0e535be7d57e9c660
Armand Kaufmann <armandkaufmann@gmail.com>
2022-12-04 23:15:50 -0400

    added validation for customer entry

--- app/src/main/java/com/example/cruddypizza/CustomerEntry.java
+++ app/src/main/java/com/example/cruddypizza/CustomerEntry.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomerEntry extends AppCompatActivity {
     //views
@@ -31,6 +32,11 @@ public class CustomerEntry extends AppCompatActivity {
 
     //customer object
     Customer customer = new Customer();
+
+    //error messages
+    String customerNameError;
+    String customerAddressError;
+    String customerPhoneError;
 
 
     @Override
@@ -75,6 +81,10 @@ public class CustomerEntry extends AppCompatActivity {
             //buttons
             buttonStartOrder.setText(R.string.buttonMakePizzaEN);
             buttonCustCancel.setText(R.string.buttonCancelEN);
+            //error messages
+            customerAddressError = getResources().getString(R.string.CustAddressErrorEN);
+            customerNameError = getResources().getString(R.string.CustNameErrorEN);
+            customerPhoneError = getResources().getString(R.string.CustPhoneErrorEN);
         }else{
             textViewCustInfoTitle.setText(R.string.CustInfoTitleFR);
             textViewName.setText(R.string.CustNameFR);
@@ -83,6 +93,10 @@ public class CustomerEntry extends AppCompatActivity {
             //buttons
             buttonStartOrder.setText(R.string.buttonMakePizzaFR);
             buttonCustCancel.setText(R.string.buttonCancelFR);
+            //error messages
+            customerAddressError = getResources().getString(R.string.CustAddressErrorFR);
+            customerNameError = getResources().getString(R.string.CustNameErrorFR);
+            customerPhoneError = getResources().getString(R.string.CustPhoneErrorFR);
         }
 
     }//end setLanguage method
@@ -91,16 +105,33 @@ public class CustomerEntry extends AppCompatActivity {
     private View.OnClickListener buttonStartOrderListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //making new customer object
-            customer.setName(editTextTextCustName.getText().toString());
-            customer.setAddress(editTextTextCustAddress.getText().toString());
-            customer.setNumber(editTextTextCustPhone.getText().toString());
+            boolean errors = false;
+            if (editTextTextCustName.getText().toString().equals("")){
+                errors = true;
+                Toast.makeText(getApplicationContext(), customerNameError, Toast.LENGTH_SHORT).show();
+            }
+            if (editTextTextCustAddress.getText().toString().equals("")){
+                errors = true;
+                Toast.makeText(getApplicationContext(), customerAddressError, Toast.LENGTH_SHORT).show();
+            }
+            if (editTextTextCustPhone.getText().toString().equals("")){
+                errors = true;
+                Toast.makeText(getApplicationContext(), customerPhoneError, Toast.LENGTH_SHORT).show();
+            }
 
-            Intent i = new Intent(CustomerEntry.this, MakePizza.class);
+            if (!errors){ //if there were no validation errors
+                //making new customer object
+                customer.setName(editTextTextCustName.getText().toString());
+                customer.setAddress(editTextTextCustAddress.getText().toString());
+                customer.setNumber(editTextTextCustPhone.getText().toString());
 
-            i.putExtra("language", language);
-            i.putExtra("customer", customer);
-            startActivity(i);
+                Intent i = new Intent(CustomerEntry.this, MakePizza.class);
+
+                i.putExtra("language", language);
+                i.putExtra("customer", customer);
+                startActivity(i);
+            }
+
         }
     };
 
